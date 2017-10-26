ee47e7ae2cdc81dd8d72b3a3a9f71661f0ccf2ac
Paul Baker <paul.nelson.baker@gmail.com>
2017-10-25 20:24:44 -0600

    Adding sanitization
    Who knows what's comming in. Jsoup let's us do some really simple sanitization, we might as well take advantage of this functionality to clean input.

--- src/test/java/HTML2AtlassianMarkupDemo.java
+++ src/test/java/HTML2AtlassianMarkupDemo.java
@@ -1,4 +1,6 @@
 import com.atlassian.renderer.wysiwyg.converter.DefaultWysiwygConverter;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ public class HTML2AtlassianMarkupDemo {
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup")
     public void testHTMLIsProperlyConverted() {
         String htmlString = "This is <em>emphasized</em> and <b>bold</b>";
+        htmlString = Jsoup.clean(htmlString, Whitelist.relaxed());
         DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
         String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
         assertEquals("This is _emphasized_ and *bold*", wikiMarkupString);
