9b47bb0fc7550128b92ea1ed4a39ab64d27bab9e
Paul Baker <paul.nelson.baker@gmail.com>
2017-10-26 18:47:05 -0600

    Adding comments
    Including attribution in javadoc and some explaination for the other test-case.

--- src/test/java/HTML2AtlassianMarkupTests.java
+++ src/test/java/HTML2AtlassianMarkupTests.java
@@ -8,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HTML2AtlassianMarkupTests {
 
+    /**
+     * Original test-case written for demo purposes by <a href="https://stackoverflow.com/users/981922/dokaspar">dokaspar</a>
+     *
+     * @see <a href="https://stackoverflow.com/a/37623940/1478636">original stackoverflow answer</a>
+     */
     @Test
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup")
     public void testHTMLIsProperlyConverted() {
@@ -20,7 +25,9 @@ public class HTML2AtlassianMarkupTests {
     @Test
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup - With JSOUP Cleaning")
     public void testHTMLIsProperlyConvertedAfterCleaned() {
+        // We're including a horizontal-rule at the end of the HTML string.
         String htmlString = "This is <em>emphasized</em> and <b>bold</b> <hr>";
+        // The <hr> will be filtered out by Jsoup's relaxed whitelist.
         htmlString = Jsoup.clean(htmlString, Whitelist.relaxed());
         DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
         String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
