b9c2a8479fe0981524ad5c26c3482c347e0ccfe1
Paul Baker <paul.baker@wgu.edu>
2017-10-26 18:32:09 -0600

    Removed failed test
    Removed the test we got to fail intentionally. We're now running with maven so we don't need to trial and error configurations to failure anymore.

--- src/test/java/HTML2AtlassianMarkupTests.java
+++ src/test/java/HTML2AtlassianMarkupTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class HTML2AtlassianMarkupTests {
 
@@ -21,16 +20,10 @@ public class HTML2AtlassianMarkupTests {
     @Test
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup - With JSOUP Cleaning")
     public void testHTMLIsProperlyConvertedAfterCleaned() {
-        String htmlString = "This is <em>emphasized</em> and <b>bold</b>";
+        String htmlString = "This is <em>emphasized</em> and <b>bold</b> <hr>";
         htmlString = Jsoup.clean(htmlString, Whitelist.relaxed());
         DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
         String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
         assertEquals("This is _emphasized_ and *bold*", wikiMarkupString);
-    }
-
-    @Test
-    @DisplayName("This test should fail in \"mvn test\", but doesn't")
-    public void testShouldFail() {
-        fail("This should fail, and does in intellij but not with \"mvn test\"");
     }
 }
