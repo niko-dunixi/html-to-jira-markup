77a37e18be848b96133ecaf7054f2ba532315b55
Paul Baker <paul.baker@wgu.edu>
2018-01-11 18:36:07 -0700

    Demo for github issue
    https://github.com/paul-nelson-baker/html-to-jira-markup/issues/2

--- src/test/java/HTML2AtlassianMarkupTests.java
+++ src/test/java/HTML2AtlassianMarkupTests.java
@@ -15,7 +15,7 @@ public class HTML2AtlassianMarkupTests {
      */
     @Test
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup")
-    public void testHTMLIsProperlyConverted() {
+    public void testHtmlIsProperlyConverted() {
         String htmlString = "This is <em>emphasized</em> and <b>bold</b>";
         DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
         String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
@@ -24,7 +24,7 @@ public class HTML2AtlassianMarkupTests {
 
     @Test
     @DisplayName("Test HTML is Properly Converted to Atlassian Markup - With JSOUP Cleaning")
-    public void testHTMLIsProperlyConvertedAfterCleaned() {
+    public void testHtmlIsProperlyConvertedAfterCleaned() {
         // We're including a horizontal-rule at the end of the HTML string.
         String htmlString = "This is <em>emphasized</em> and <b>bold</b> <hr>";
         // The <hr> will be filtered out by Jsoup's relaxed whitelist.
@@ -32,5 +32,44 @@ public class HTML2AtlassianMarkupTests {
         DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
         String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
         assertEquals("This is _emphasized_ and *bold*", wikiMarkupString);
+    }
+
+    /**
+     * Written as a demo for the github issue:
+     *
+     * @see <a href=https://github.com/paul-nelson-baker/html-to-jira-markup/issues/2">Github Issue</a>
+     */
+    @Test
+    @DisplayName("Test HTML without JSoup")
+    public void testHtmlToMarkupWithoutJSoup() {
+        String htmlString = "<table>\n" +
+                "<tr>\n" +
+                "  <th>First Name</th>\n" +
+                "  <th>Last Name</th>\n" +
+                "  <th>Age</th>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "  <td>Jill</td>\n" +
+                "  <td>Smith</td>\n" +
+                "  <td>50</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "  <td>Eve</td>\n" +
+                "  <td>Jackson</td>\n" +
+                "  <td>94</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "  <td>John</td>\n" +
+                "  <td>Doe</td>\n" +
+                "  <td>80</td>\n" +
+                "</tr>\n" +
+                "</table>";
+        htmlString = Jsoup.clean(htmlString, Whitelist.relaxed());
+        DefaultWysiwygConverter converter = new DefaultWysiwygConverter();
+        String wikiMarkupString = converter.convertXHtmlToWikiMarkup(htmlString);
+        assertEquals("|| First Name || Last Name || Age ||\n" +
+                "| Jill | Smith | 50 |\n" +
+                "| Eve | Jackson | 94 |\n" +
+                "| John | Doe | 80 |", wikiMarkupString);
     }
 }
